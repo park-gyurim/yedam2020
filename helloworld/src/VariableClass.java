d75b7b4627085fa3229568b123703c5039e67571
User <User@YD02-08>
2020-01-22 10:39:32 +0900

    두번째 수업

---
+++ helloworld/src/VariableClass.java
@@ -0,0 +1,15 @@
+																										
+public class VariableClass {
+	public static void main(String[] args) {
+	
+	int num1, num2, num3;
+	
+    num1 = 100; 
+	num2 = 90;
+	num3 = 80;
+	
+	int sum = num1 + num2 + num3;
+	
+	System.out.println("세수의 합계는" + sum);
+	}
+}
---
+++ helloworld/src/helloworld/Helloworld.java
@@ -0,0 +1,40 @@
+package helloworld;
+public class Helloworld {
+	/*
+	 * ���������� �ּ�ó���Ҷ� ����մϴ�. �ۼ���: �ڱԸ� 01.21
+	 */
+	public static void main(String[] args) {
+		// helloworld ����ϴ� ����.
+			int age = 25;
+			int year = 2020; //(year 변수 선언하다라는뜻 , 변수를 초기화하다)
+		System.out.println("Hello, Welcome to the java world!");
+		System.out.println("���ش�" + year + "��");
+		System.out.println("�� ���̴�" + age + "�Դϴ�");
+		// System.out.println("�ڹٴ� ��մ� ���")
+		
+	long height = 17000000000L; //int형 데이터타입이다. (== 같다라는뜻, = 출력하다라는뜻)
+	int weight = 49;
+	// int : 타입, height&weight : 변수
+	//1바이트 byte 2바이트 shout 4바이트 int 8바이트 long : 정수형 타입
+	
+	short year1 = 2021; // 변수의 값은 한번만 선언가능하다.
+	
+	int firstDayOfYear = 1;
+	int firstdayofyeat = 1;
+	
+	int num1, num2, num3;
+	 num1 = 10;
+	 num2 = 20;
+	
+	int summary = num1 + num2;
+	
+	long sum = 0;
+	int firstValue = 2000000000;
+	int secondValue = 1000000000;
+	sum = (long)firstValue + secondValue;
+	
+	System.out.println("두수의 합은:" + sum);
+	System.out.println("int 최소값: " + ". class");
+	System.out.println("int 최대값: " + ". class");
+	}
+}
